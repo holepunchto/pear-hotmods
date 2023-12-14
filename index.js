4cd2560b9c46baedcff46d6f88a6429db037fb17
David Mark Clements <david.mark.clements@gmail.com>
2023-12-14 14:06:52 +0100

    🥵

---
+++ index.js
@@ -0,0 +1,54 @@
+import { updates } from 'pear'
+
+const { origin } = new URL(import.meta.url)
+const mods = {} 
+
+export function hotmods (opts, listener) {
+  if (typeof opts === 'function') {
+    listener = opts
+    opts = {}
+  }
+  if (typeof listener !== 'function') {
+    throw new Error('pear-hotmods: Supply a listener function')
+  }
+  const { paths = [] }  = opts
+
+  updates(async ({ diff }) => {
+    for (const key of diff) {
+      if (key.endsWith('.js') === false) continue
+      if (paths?.length && paths.some((path) => key.startsWith(path)) === false) continue
+      
+      mods[key] = mods[key] || {
+        original: null,
+        previous: null,
+        next: null,
+        reloads: null
+      }
+      mods[key].original = mods[key].original || await import(origin + key + '+app+esm')
+      mods[key].previous = mods[key].next || mods[key].original
+      try {
+        mods[key].next = await import(origin + key + `?refresh=${Date.now()}+app+esm`)
+      } catch (err) {
+        console.error('Error live-reloading', key, err)
+      }
+      mods[key].reloads = Array.from(new Set(Object.keys(mods[key].original)
+        .concat(Object.keys(mods[key].previous))))
+        .map((name) => ({
+          original: mods[key].original[name],
+          previous: mods[key].previous[name],
+          next: mods[key]?.next[name]
+        }))
+    }
+    
+    const reloads = Array.from(new Set(Object.values(mods).reduce((reloads, hotmod) => {
+      reloads.push(...hotmod.reloads)
+      return reloads
+    }, [])))
+
+    await listener(reloads)
+  })
+
+}
+
+
+export default hotmods
---
+++ readme.md
@@ -0,0 +1,51 @@
+# pear-hotmods
+
+The `pear-hotmods` library is a Pear ecosystem library. 
+It automatically reloads Module instances which can be passed on to a live-reload mechanism commonly supplied by frontend-frameworks.
+
+## Install
+
+```
+npm install pear-hotmods
+```
+
+## API
+
+```js
+const { hotmods } = await import('pear-hotmods')
+```
+
+```js
+import hotmods from 'pear-hotmods'
+```
+
+### `hotmods([options, ] listener <Async Function|Function>)`
+
+Call the `listener` function 
+
+**Options**
+
+* `paths` `<Array>` - paths allowlist to match against. Paths are drive keys: they are rooted to application root. If unset or empty array, matches against all paths.
+
+## Example
+
+Scenario where Preact framework is being used with prefresh for hot-reloading during
+application development:
+
+```js
+import { config } from 'pear'
+if (config.dev && config.watch) {
+  const { hotmods } = await import('pear-hotmods')
+  await import('@prefresh/core')
+  hotmods({ paths: ['/components'] }, (reloads) => {
+    for (const { original, previous, next } of reloads) {
+      window.__PREFRESH__.replaceComponent(original, next, true)
+      window.__PREFRESH__.replaceComponent(previous, next, true)
+    }
+  })
+}
+```
+
+## License
+
+Apache-2.0 
