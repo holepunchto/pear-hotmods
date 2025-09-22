9c2f93c7f31e14fa03022a9be70229eb9fb50d21
Mikkel Malmberg <mikkel@brnbw.com>
2025-09-22 15:58:50 +0200

    Apply Prettier

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { origin } = new URL(import.meta.url)
 const mods = {}
 
-export function hotmods (opts, listener) {
+export function hotmods(opts, listener) {
   if (!global.Pear) return
 
   const { updates } = global.Pear
@@ -21,7 +21,8 @@ export function hotmods (opts, listener) {
       if (typeof update === 'string') update = { type: 'update', key: update } // back compat
       const { type, key } = update
       if (key.endsWith('.js') === false) continue
-      if (paths?.length && paths.some((path) => key.startsWith(path)) === false) continue
+      if (paths?.length && paths.some((path) => key.startsWith(path)) === false)
+        continue
 
       mods[key] = mods[key] || {
         original: null,
@@ -29,32 +30,43 @@ export function hotmods (opts, listener) {
         next: null,
         reloads: null
       }
-      mods[key].original = mods[key].original || await import(origin + key + '+app+esm')
+      mods[key].original =
+        mods[key].original || (await import(origin + key + '+app+esm'))
       mods[key].previous = mods[key].next || mods[key].original
       try {
-        mods[key].next = await import(origin + key + `?refresh=${Date.now()}+app+esm`)
+        mods[key].next = await import(
+          origin + key + `?refresh=${Date.now()}+app+esm`
+        )
       } catch (err) {
         console.error('Error live-reloading', key, err)
       }
-      mods[key].reloads = Array.from(new Set(Object.keys(mods[key].original)
-        .concat(Object.keys(mods[key].previous))))
-        .map((name) => ({
-          type,
-          key,
-          original: mods[key].original[name],
-          previous: mods[key].previous[name],
-          next: mods[key]?.next[name]
-        }))
+      mods[key].reloads = Array.from(
+        new Set(
+          Object.keys(mods[key].original).concat(
+            Object.keys(mods[key].previous)
+          )
+        )
+      ).map((name) => ({
+        type,
+        key,
+        original: mods[key].original[name],
+        previous: mods[key].previous[name],
+        next: mods[key]?.next[name]
+      }))
     }
 
-    const reloads = Array.from(new Set(Object.values(mods).reduce((reloads, hotmod) => {
-      if (Array.isArray(hotmod.reloads)) {
-        reloads.push(...hotmod.reloads)
-      }
-      return reloads
-    }, [])))
+    const reloads = Array.from(
+      new Set(
+        Object.values(mods).reduce((reloads, hotmod) => {
+          if (Array.isArray(hotmod.reloads)) {
+            reloads.push(...hotmod.reloads)
+          }
+          return reloads
+        }, [])
+      )
+    )
 
-    await listener(reloads, { })
+    await listener(reloads, {})
   })
 }
 
